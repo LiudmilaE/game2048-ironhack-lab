d1c84c18d837ee27fe037871fda0d73136be3fc5
Liudmyla Iefremova <liudmyla.iefremova@hotmail.com>
2017-09-14 10:37:47 +0200

    Initial commit

---
+++ js/game2048.js
@@ -0,0 +1,13 @@
+function Game2048 () {
+  this.board = [
+    [null,null,null,null],
+    [null,null,null,null],
+    [null,null,null,null],
+    [null,null,null,null]
+  ];
+
+  this.score = 0;
+  this.won   = false;
+  this.lost  = false;
+  
+}
