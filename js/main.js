a74d1594181ba8024ebfa4837691e26048b9db1a
Liudmyla Iefremova <liudmyla.iefremova@hotmail.com>
2017-09-14 15:20:22 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -1,4 +1,73 @@
 console.log("Hello main");
 
-var game = new Game2048();
-game._renderBoard();
+var game;
+
+window.onload = function () {
+  game = new Game2048();
+  //game._renderBoard();
+  renderTiles();
+};
+
+function renderTiles () {
+  game.board.forEach(function(row, rowIndex){
+    row.forEach(function (cell, cellIndex) {
+      if (cell) {
+        var tileContainer = document.getElementById("tile-container");
+        var newTile       = document.createElement("div");
+
+        newTile.classList  = "tile val-" + cell;
+        newTile.classList += " tile-position-" + rowIndex + "-" + cellIndex;
+        newTile.innerHTML  = (cell);
+
+        tileContainer.appendChild(newTile);
+      }
+    });
+  });
+}
+
+function updateScore () {
+  var score          = game.score;
+  var scoreContainer = document.getElementsByClassName("js-score");
+
+  Array.prototype.slice.call(scoreContainer).forEach(function (span) {
+    span.innerHTML = score;
+  });
+}
+
+function moveListeners (event) {
+  var keys = [37, 38, 39, 40];
+
+  if (keys.indexOf(event.keyCode) < 0)
+    return;
+
+  switch (event.keyCode) {
+    case 37: game.move("left");  break;
+    case 38: game.move("up");    break;
+    case 39: game.move("right"); break;
+    case 40: game.move("down");  break;
+  }
+
+  resetTiles();
+  renderTiles();
+  updateScore();
+  gameStatus();
+}
+
+document.addEventListener("keydown", moveListeners);
+
+
+function resetTiles () {
+  var tilesContainer = document.getElementById("tile-container");
+  var tiles          = tilesContainer.getElementsByClassName("tile");
+
+  Array.prototype.slice.call(tiles).forEach(function (tile) {
+    tilesContainer.removeChild(tile);
+  });
+}
+function gameStatus () {
+  if (game.win()) {
+    document.getElementById("game-over").classList = "show-won";
+  } else if (game.lose()) {
+    document.getElementById("game-over").classList = "show-lost";
+  }
+}
